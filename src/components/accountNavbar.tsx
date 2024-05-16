e7bd6695210b55a41f313123207a16c1cfd088bb
Ploy <117616170+yaminP@users.noreply.github.com>
2024-05-16 10:48:23 +0700

    navbar

---
+++ src/components/accountNavbar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { BiUser } from 'react-icons/bi';
+import Link from 'next/link';
+
+const AccoutNavbar: React.FC = () => {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <div className="relative">
+            <div className="flex-none h-full text-center flex items-center justify-center">
+                <button onClick={toggleDropdown} className='hover:text-red-700'>
+                    <BiUser />
+                </button>
+            </div>
+            {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-44 bg-white border rounded-md shadow-lg z-50">
+                    <ul className="py-1">
+                        <li className="px-4 py-2 hover:bg-gray-950 hover:text-white cursor-pointer">
+                            <Link href="/profile">Profile</Link>
+                        </li>
+                        <li className="px-4 py-2 hover:bg-gray-950 hover:text-white cursor-pointer">
+                            <Link href="/logout">Logout</Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AccoutNavbar;
