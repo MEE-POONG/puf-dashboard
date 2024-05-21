d6f84ad8ae4e6ec4d431228fd7768f021c95f9ed
devilzeros <devilzeros@outlook.co.th>
2024-05-21 11:32:57 +0700

    est

--- src/pages/agent/index.tsx
+++ src/pages/agent/index.tsx
@@ -1,10 +1,9 @@
-import DashboardLayout from "@/components/Layout";
-import AddPartnerModal from "@/components/Partner/AddPartnerModal";
-import { AllianceDataList } from "@/data";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { CiEdit, CiTrash } from "react-icons/ci";
+import ModalAllianceAdd from "@/components/alliance/ModalAllianceAdd";
+import DashboardLayout from "@/components/Layout";
+import axios from "axios";
 import { FaSearch } from "react-icons/fa";
+import PageSelect from "@/components/PageSelect";
 
 interface Partner {
     id: number;
@@ -17,8 +16,19 @@ interface Partner {
     line: string;
     allianceId: string;
 }
-
+interface Params {
+    page: number;
+    pageSize: number;
+    search: string;
+    totalPages: number;
+}
 const AgentPage: React.FC = () => {
+    const [params, setParams] = useState<Params>({
+        page: 1,
+        pageSize: 10,
+        search: "",
+        totalPages: 1,
+    });
     const [partners, setPartners] = useState<Partner[]>([]);
     const [filteredPartners, setFilteredPartners] = useState<Partner[]>([]);
     const [searchQuery, setSearchQuery] = useState('');
@@ -48,6 +58,20 @@ const AgentPage: React.FC = () => {
 
     const handleAddPartner = (data: any) => {
         console.log(data);
+    };
+    const handleChangePage = (page: number) => {
+        setParams((prevParams) => ({
+            ...prevParams,
+            page: page,
+        }));
+    };
+
+    const handleChangePageSize = (size: number) => {
+        setParams((prevParams) => ({
+            ...prevParams,
+            page: 1,
+            pageSize: size,
+        }));
     };
 
     return (
@@ -82,7 +106,7 @@ const AgentPage: React.FC = () => {
                 <div className="mt-5 md:mt-0">
                     <button onClick={() => setShowModal(true)} className="px-3 py-1 bg-teal-500 text-white rounded-full hover:bg-teal-700 text-sm">Add UserAgent</button>
                 </div>
-                <AddPartnerModal show={showModal} onClose={() => setShowModal(false)} onSubmit={handleAddPartner} />
+                <ModalAllianceAdd show={showModal} onClose={() => setShowModal(false)} onSubmit={handleAddPartner} />
             </div>
             <div className="flex min-h-full items-center justify-center shadow-md rounded-xl  overflow-hidden">
                 <div className="overflow-x-auto w-full">
@@ -99,7 +123,7 @@ const AgentPage: React.FC = () => {
                             </tr>
                         </thead>
                         <tbody className="text-blue-gray-900">
-                            {AllianceDataList.map((list, index) => (
+                            {/* {AllianceDataList.map((list, index) => (
                                 <tr className="border-b border-blue-gray-200" key={list.userAccount}>
                                     <td className="py-3 px-4">{index + 1}</td>
                                     <td className="py-3 px-4">{list.userAccount}</td>
@@ -112,7 +136,7 @@ const AgentPage: React.FC = () => {
                                         <a href="#" className="font-medium text-red-600 hover:text-blue-800"><CiTrash /></a>
                                     </td>
                                 </tr>
-                            ))}
+                            ))} */}
                             {/* {filteredPartners.map((partner, index) => (
                                     <tr className="border-b border-blue-gray-200" key={partner.id}>
                                         <td className="py-3 px-4">{index + 1}</td>
@@ -134,44 +158,7 @@ const AgentPage: React.FC = () => {
                 </div>
             </div>
             <div className="card-footer">
-                <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between py-4" aria-label="Table navigation">
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-                        Showing
-                        <span className="font-semibold text-gray-900 dark:text-white mx-2">1-10</span>
-                        of
-                        <select
-                            className=" mx-2 items-center justify-center px-3 h-8  text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                            <option selected value="10">10</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                            <option value="500">500</option>
-                            <option value="1000">1000</option>
-                        </select>
-                    </span>
-                    <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                        </li>
-                        <li>
-                            <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-                        </li>
-                    </ul>
-                </nav>
+                <PageSelect page={params?.page} totalPages={params?.totalPages} onChangePage={handleChangePage} onChangePageSize={handleChangePageSize} />
             </div>
         </DashboardLayout>
     );
