3cc4d09d0279ff3a295a66e02718bd0ce8f81c2c
devilzeros <devilzeros@outlook.co.th>
2024-05-21 17:07:35 +0700

    esat

--- src/components/alliance/ModalAllianceAdd.tsx
+++ src/components/alliance/ModalAllianceAdd.tsx
@@ -59,7 +59,7 @@ const ModalAllianceAdd: React.FC<AddPartnerModalProps> = ({ show, onClose, onSub
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-    
+
         // Basic validation
         if (!formData.position) {
             setError("กรุณาเลือกตำแหน่ง");
@@ -73,7 +73,7 @@ const ModalAllianceAdd: React.FC<AddPartnerModalProps> = ({ show, onClose, onSub
             setError("กรุณาระบุยูสพันธมิตร");
             return;
         }
-    
+
         // Check if userAccount is unique
         try {
             const checkResponse = await axios.get<CheckUserAccountResponse>(`/api/alliance/checkUserAccount`, { params: { userAccount: formData.userAccount } });
@@ -86,7 +86,7 @@ const ModalAllianceAdd: React.FC<AddPartnerModalProps> = ({ show, onClose, onSub
             setError('An error occurred while checking the user account');
             return;
         }
-    
+
         // Submit form
         try {
             const response = await axios.post('/api/alliance', formData);
@@ -125,11 +125,11 @@ const ModalAllianceAdd: React.FC<AddPartnerModalProps> = ({ show, onClose, onSub
                         </div>
                         <div>
                             <label htmlFor="userAccount" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ยูสเซอร์ AG</label>
-                            <input type="text" id="userAccount" name="userAccount" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ระบุยูสพันธมิตร" required value={formData.userAccount} onChange={handleChange} disabled={formData?.position === ""} />
+                            <input type="text" id="userAccount" name="userAccount" className={` border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${formData?.position === "" ? `bg-gray-50 dark:bg-gray-700` : `bg-white dark:bg-white`} dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="ระบุยูสพันธมิตร" required value={formData.userAccount} onChange={handleChange} disabled={formData?.position === ""} />
                         </div>
                         <div>
                             <label htmlFor="percent" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ยอดสู้</label>
-                            <input type="number" min={0} max={40} id="percent" name="percent" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ระบุสู้ฟรี" required value={formData.percent} onChange={handleChange} disabled={formData?.position === ""} />
+                            <input type="number" min={0} max={40} id="percent" name="percent" className={` border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${formData?.position === "" ? `bg-gray-50 dark:bg-gray-700` : `bg-white dark:bg-white`} dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="ระบุสู้ฟรี" required value={formData.percent} onChange={handleChange} disabled={formData?.position === ""} />
                         </div>
                         <div>
                             <label>
